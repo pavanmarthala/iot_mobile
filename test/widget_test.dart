aedf8004a941923c367282cf7305a8836c9b759a
pavan <pavanmarthala36@gmail.com>
2023-11-22 13:22:58 +0530

    added

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,6 +5,8 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -13,7 +15,7 @@ import 'package:iot_mobile_app/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp(languages: {},));
+    await tester.pumpWidget(  MyApp(languages: {},));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
